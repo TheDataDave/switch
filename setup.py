17c4680e7a15b87438dadd94f328e2b1299927a4
TheDataDave <156825340+TheDataDave@users.noreply.github.com>
2024-05-15 21:24:07 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='switch',
+    version='0.2.0',  # Update version to reflect new implementation
+    description='A simple and flexible switch statement implementation for Python.',
+    author='David Flanders',
+    author_email='thedatadave@gmail.com',
+    url='https://github.com/TheDataDave/switch',
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 4 - Beta',  # Update development status
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+)
+
